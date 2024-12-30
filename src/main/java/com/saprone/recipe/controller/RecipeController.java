b68465c8616cffbb0256a8e9c3b21fac6bcab1ba
Bas <onrust.bas@gmail.com>
2024-12-30 15:22:53 +0100

    added crossorigin localhost

--- src/main/java/com/saprone/recipe/controller/RecipeController.java
+++ src/main/java/com/saprone/recipe/controller/RecipeController.java
@@ -4,14 +4,12 @@ import com.saprone.recipe.model.Recipe;
 import com.saprone.recipe.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/recipes")
+@CrossOrigin(origins = "http://localhost:8085")
 public class RecipeController {
 
     private final RecipeService recipeService;
